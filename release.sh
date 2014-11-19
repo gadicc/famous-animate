6ecc0e0a460a5de4df52a1782fbd7ca713223e3a
Jens Zastrow <jens.zastrow@mj-networks-gmbh.de>
2014-11-19 17:05:35 +0100

    ddd

--- release.sh
+++ release.sh
@@ -3,6 +3,11 @@ msg=$1; if [ -z "${msg}" ]; then echo "commit message mandatory"; exit 3; fi
 
 if [ $(git status | grep -c "Untracked") = 1 ]; then git status; exit 3; fi
 
+version=$(cat package.js | grep -Po 'version : "\K[^"]*')
+minorVersion=$(echo ${version} | cut -d '.' -f 3)
+
+echo "V: $version m: $minorVersion"
+
 #spacejam -test-packages ./
 git commit -m "${msg}" -a
 git push
