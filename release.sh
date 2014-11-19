6bf4c35f698e5d5c24250652877fae33f2b2d7fb
Jens Zastrow <jens.zastrow@mj-networks-gmbh.de>
2014-11-19 17:10:24 +0100

    ddd

--- release.sh
+++ release.sh
@@ -5,8 +5,11 @@ if [ $(git status | grep -c "Untracked") = 1 ]; then git status; exit 3; fi
 
 version=$(cat package.js | grep -Po 'version : "\K[^"]*')
 minorVersion=$(echo ${version} | cut -d '.' -f 3)
+newVersion="$(echo ${version} | cut -d '.' -f 1-2).$(($minorVersion + 1))"
 
-echo "V: $version m: $minorVersion"
+echo "V: $version m: $minorVersion n:$newVersion"
+
+
 
 #spacejam -test-packages ./
 git commit -m "${msg}" -a
