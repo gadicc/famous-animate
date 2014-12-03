33c78d227e1fa605beeacf126b18a598044412ac
Jens Zastrow <jens.zastrow@mj-networks-gmbh.de>
2014-12-03 09:01:27 +0000

    famono fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: 'provides #Animate view',
-  version : "0.0.11",
+  version : "0.0.12",
   name : 'mjn:fview-animate',
   git : 'https://github.com/mj-networks/famous-animate.git'
 });
